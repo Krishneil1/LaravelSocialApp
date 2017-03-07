7a58f86af700464f4c4a80947c167b83fd0b70ed
Krishneel <Krishneil1@gmail.com>
2017-03-07 20:00:52 +1300

    Showing the timeline page

--- chatty/app/Http/Controllers/HomeController.php
+++ chatty/app/Http/Controllers/HomeController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace Chatty\Http\Controllers;
+use Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        if(Auth::check())
+        {
+            return view('timeline.index');
+        }
         return view ('home');
     }
 }
